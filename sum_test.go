b2af24acb1c4d2f21a5e0bb84cee87208d81eae9
Tiago rosa da costa <tiagorosadacost@gmail.com>
2021-10-17 00:04:13 -0300

    Add test to sub, multiple and divise

--- sum_test.go
+++ sum_test.go
@@ -10,3 +10,24 @@ func TestSum(t *testing.T) {
 		t.Errorf("Return is invalid. The correct value is %d", result)
 	}
 }
+
+func TestSub(t *testing.T) {
+	result := sub(10, 10)
+	if result != 0 {
+		t.Errorf("Return is invalid. The correct value is %d", result)
+	}
+}
+
+func TestMultiple(t *testing.T) {
+	result := multiple(10, 10)
+	if result != 100 {
+		t.Errorf("Return is invalid. The correct value is %d", result)
+	}
+}
+
+func TestDivise(t *testing.T) {
+	result := divise(10, 10)
+	if result != 1 {
+		t.Errorf("Return is invalid. The correct value is %d", result)
+	}
+}
