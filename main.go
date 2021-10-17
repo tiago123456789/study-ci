1ff4ff2d46e2f3f5a8df4f0dfd004ca7ebf41050
Tiago rosa da costa <tiagorosadacost@gmail.com>
2021-10-16 23:46:09 -0300

    Add another methods

--- main.go
+++ main.go
@@ -6,6 +6,18 @@ func sum(a int, b int) int {
 	return a + b
 }
 
+func sub(a int, b int) int {
+	return a - b
+}
+
+func multiple(a int, b int) int {
+	return a * b
+}
+
+func divise(a int, b int) int {
+	return a / b
+}
+
 func main() {
 	fmt.Printf("THe value sum method => %d \n", sum(10, 10))
 }
